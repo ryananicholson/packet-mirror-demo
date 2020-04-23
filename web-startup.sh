f1b5b2a695927ff93d9f3d1615eb16165d501f08
ryananicholson <ryananicholson@gmail.com>
2020-04-23 15:00:48 -0400

    Adjusted other web server discovery

--- web-startup.sh
+++ web-startup.sh
@@ -42,24 +42,20 @@ sudo sed -i 's/#net\.ipv4\.ip_forward=1/net\.ipv4\.ip_forward=1/g' /etc/sysctl.c
 sudo sysctl -p
 MYIP=$(ifconfig ens4 | grep inet| awk '{print $2}' | head -1)
 FIRSTTHREE=$(echo $MYIP | cut -d '.' -f1-3)
-LASTOCT=$(expr $(echo $MYIP | cut -d '.' -f4) - 1)
-TESTIP=$FIRSTTHREE"."$LASTOCT
-OTHERWS=""
-echo -e "test\n\n" | nc $TESTIP 80 -w1 >/dev/null
-if [ $? -eq 0 ]; then
-  OTHERWS=$TESTIP
-fi
 
-if ! [[ $OTHERWS == $TESTIP ]]; then
-  LASTOCT=$(expr $(echo $MYIP | cut -d '.' -f4) + 1)
-  TESTIP=$FIRSTTHREE"."$LASTOCT
-  echo -e "test\n\n" | nc $TESTIP 80 -w1 >/dev/null
-  if [ $? -eq 0 ]; then
-    OTHERWS=$TESTIP
-  fi
-fi
+for i in $(seq 2 254); do
+    TESTIP="$FIRSTTHREE.$i"
+    if ! [ $TESTIP == $MYIP ]; then
+        if [[ $(echo -e "GET / HTTP/1.1\n\n" | nc -w 1 $TESTIP 80) ]]; then
+            FOUND=1
+            break
+        fi
+    fi
+done
 
-sudo iptables -t nat -A PREROUTING -s 35.191.0.0/16 -p tcp --dport 80 -j DNAT --to-destination $OTHERWS
-sudo iptables -t nat -A PREROUTING -s 130.211.0.0/22 -p tcp --dport 80 -j DNAT --to-destination $OTHERWS
-sudo iptables -t nat -A POSTROUTING -j MASQUERADE
-sudo iptables save
+if [ $FOUND -eq 1 ]; then
+    sudo iptables -t nat -A PREROUTING -s 35.191.0.0/16 -p tcp --dport 80 -j DNAT --to-destination $TESTIP
+    sudo iptables -t nat -A PREROUTING -s 130.211.0.0/22 -p tcp --dport 80 -j DNAT --to-destination $TESTIP
+    sudo iptables -t nat -A POSTROUTING -j MASQUERADE
+    sudo iptables-save
+fi
